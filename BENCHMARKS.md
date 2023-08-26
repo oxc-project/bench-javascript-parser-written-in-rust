234b4fad9cb930c9cb4181b45f45934338df0fc2
Boshen <boshenc@gmail.com>
2023-08-26 15:44:53 +0800

    update

---
+++ BENCHMARKS.md
@@ -0,0 +1,20 @@
+# Benchmarks
+
+## Table of Contents
+
+- [Benchmark Results](#benchmark-results)
+    - [typescript.js](#typescript.js)
+
+## Benchmark Results
+
+### typescript.js
+
+|                     | `oxc`                    | `swc`                            | `rome`                            |
+|:--------------------|:-------------------------|:---------------------------------|:--------------------------------- |
+| **`single-thread`** | `55.86 ms` (✅ **1.00x**) | `108.68 ms` (❌ *1.95x slower*)   | `168.88 ms` (❌ *3.02x slower*)    |
+| **`no-drop`**       | `56.17 ms` (✅ **1.00x**) | `101.47 ms` (❌ *1.81x slower*)   | `159.46 ms` (❌ *2.84x slower*)    |
+| **`parallel`**      | `90.12 ms` (✅ **1.00x**) | `180.26 ms` (❌ *2.00x slower*)   | `315.39 ms` (❌ *3.50x slower*)    |
+
+---
+Made with [criterion-table](https://github.com/nu11ptr/criterion-table)
+
--- benches/parser.rs
+++ benches/parser.rs
@@ -1,88 +1,95 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{
+    criterion_group, criterion_main, measurement::Measurement, BenchmarkGroup, BenchmarkId,
+    Criterion,
+};
 use rayon::prelude::*;
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
-fn oxc_parse(source: &str) -> oxc_allocator::Allocator {
-    let allocator = oxc_allocator::Allocator::default();
-    let source_type = oxc_span::SourceType::default();
-    _ = oxc_parser::Parser::new(&allocator, source, source_type).parse();
-    allocator
+trait Bencher {
+    type ParseOutput;
+
+    const ID: &'static str;
+
+    fn parse(source: &str) -> Self::ParseOutput;
+
+    fn bench<T: Measurement>(group: &mut BenchmarkGroup<T>, source: &str) {
+        let cpus = num_cpus::get_physical();
+        let id = BenchmarkId::new(Self::ID, "single-thread");
+        group.bench_with_input(id, &source, |b, source| b.iter(|| Self::parse(source)));
+
+        let id = BenchmarkId::new(Self::ID, "no-drop");
+        group.bench_with_input(id, &source, |b, source| {
+            b.iter_with_large_drop(|| Self::parse(source))
+        });
+
+        let id = BenchmarkId::new(Self::ID, "parallel");
+        group.bench_with_input(id, &source, |b, source| {
+            b.iter(|| {
+                (0..cpus).into_par_iter().for_each(|_| {
+                    Self::parse(source);
+                });
+            })
+        });
+    }
 }
 
-fn swc_parse(source: &str) -> swc_ecma_parser::PResult<swc_ecma_ast::Module> {
-    use swc_ecma_parser::{Parser, StringInput, Syntax};
-    Parser::new(
-        Syntax::Es(Default::default()),
-        StringInput::new(source, Default::default(), Default::default()),
-        None,
-    )
-    .parse_module()
+struct OxcBencher;
+
+impl Bencher for OxcBencher {
+    type ParseOutput = oxc_allocator::Allocator;
+
+    const ID: &'static str = "oxc";
+
+    fn parse(source: &str) -> Self::ParseOutput {
+        let allocator = oxc_allocator::Allocator::default();
+        let source_type = oxc_span::SourceType::default();
+        _ = oxc_parser::Parser::new(&allocator, source, source_type).parse();
+        allocator
+    }
 }
 
-fn rome_parse(source: &str) -> rome_js_parser::Parse<rome_js_syntax::JsModule> {
-    rome_js_parser::parse_module(source)
+struct SwcBencher;
+
+impl Bencher for SwcBencher {
+    type ParseOutput = swc_ecma_parser::PResult<swc_ecma_ast::Module>;
+
+    const ID: &'static str = "swc";
+
+    fn parse(source: &str) -> Self::ParseOutput {
+        use swc_ecma_parser::{Parser, StringInput, Syntax};
+        Parser::new(
+            Syntax::Es(Default::default()),
+            StringInput::new(source, Default::default(), Default::default()),
+            None,
+        )
+        .parse_module()
+    }
+}
+
+struct RomeBencher;
+
+impl Bencher for RomeBencher {
+    type ParseOutput = rome_js_parser::Parse<rome_js_syntax::JsModule>;
+
+    const ID: &'static str = "rome";
+
+    fn parse(source: &str) -> Self::ParseOutput {
+        rome_js_parser::parse_module(source)
+    }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let cpus = num_cpus::get_physical();
     let filename = "typescript.js";
+
     let source = std::fs::read_to_string(filename).unwrap();
 
     let mut group = c.benchmark_group(filename);
-    group.throughput(Throughput::Bytes(source.len() as u64));
 
-    let id = "single-thread";
-    group.bench_with_input(BenchmarkId::new(id, "oxc"), &source, |b, source| {
-        b.iter(|| oxc_parse(source))
-    });
-
-    group.bench_with_input(BenchmarkId::new(id, "swc"), &source, |b, source| {
-        b.iter(|| swc_parse(source))
-    });
-
-    group.bench_with_input(BenchmarkId::new(id, "rome"), &source, |b, source| {
-        b.iter(|| rome_parse(source))
-    });
-
-    let id = "single-thread-no-drop";
-    group.bench_with_input(BenchmarkId::new(id, "oxc"), &source, |b, source| {
-        b.iter_with_large_drop(|| oxc_parse(source))
-    });
-
-    group.bench_with_input(BenchmarkId::new(id, "swc"), &source, |b, source| {
-        b.iter_with_large_drop(|| swc_parse(source))
-    });
-
-    group.bench_with_input(BenchmarkId::new(id, "rome"), &source, |b, source| {
-        b.iter_with_large_drop(|| rome_parse(source))
-    });
-
-    let id = "multi-thread";
-    group.bench_with_input(BenchmarkId::new(id, "oxc"), &source, |b, source| {
-        b.iter(|| {
-            (0..cpus).into_par_iter().for_each(|_| {
-                oxc_parse(source);
-            });
-        })
-    });
-
-    group.bench_with_input(BenchmarkId::new(id, "swc"), &source, |b, source| {
-        b.iter(|| {
-            (0..cpus).into_par_iter().for_each(|_| {
-                _ = swc_parse(source);
-            });
-        });
-    });
-
-    group.bench_with_input(BenchmarkId::new(id, "rome"), &source, |b, source| {
-        b.iter(|| {
-            (0..cpus).into_par_iter().for_each(|_| {
-                rome_parse(source);
-            });
-        });
-    });
+    OxcBencher::bench(&mut group, &source);
+    SwcBencher::bench(&mut group, &source);
+    RomeBencher::bench(&mut group, &source);
 
     group.finish();
 }
