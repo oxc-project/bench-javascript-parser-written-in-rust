46f45aa229fbec6a81a110cb83f87295cfa8e33c
Boshen <boshenc@gmail.com>
2024-06-11 21:09:34 +0800

    feat: add binaries

---
+++ src/biome.rs
@@ -0,0 +1,10 @@
+use std::{env, fs, path::Path};
+
+use bench_parser::biome;
+
+pub fn main() {
+    let path = env::args().nth(1).unwrap();
+    let path = Path::new(&path);
+    let source_text = fs::read_to_string(path).unwrap();
+    let _ = biome::parse(path, &source_text);
+}
---
+++ src/oxc.rs
@@ -0,0 +1,10 @@
+use std::{env, fs, path::Path};
+
+use bench_parser::oxc;
+
+pub fn main() {
+    let path = env::args().nth(1).unwrap();
+    let path = Path::new(&path);
+    let source_text = fs::read_to_string(path).unwrap();
+    let _ = oxc::parse(path, &source_text);
+}
